c0293bb3af60fc79af98f5b456fddf976ca1c1af
Vít Rozsíval <mail@vitrozsival.cz>
2020-01-07 13:41:56 +0100

    Update services config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,5 @@
 use Wavevision\NetteTests\Configuration;
 use Wavevision\NetteTests\TestAppTests\Bootstrap;
 
-require __DIR__ . '/../../../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 Configuration::setup([Bootstrap::class, 'boot']);
